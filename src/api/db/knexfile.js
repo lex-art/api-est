94ef2dafa9539c54d5798b1b9d49e9bd46b6bbdc
lex-art <drummerkoka@gmail.com>
2022-01-07 23:16:05 -0600

    config DB

--- src/api/db/knexfile.js
+++ src/api/db/knexfile.js
@@ -14,7 +14,12 @@ module.exports = {
 
   production: {
     client: "mysql",
-    connection: process.env.DATABASE_URL,
+    connection: {
+      host: process.env.DB_HOST,
+			user: process.env.DB_USER,
+			password: process.env.DB_PASSWORD,
+			database: process.env.DB_NAME,
+    },
     pool: {
       min: 1,
       max: 1
